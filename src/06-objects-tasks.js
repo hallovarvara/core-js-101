31b1b9edf1420e6e19417066fc2eafc0bd54e99d
Varya Deviaterikova <hallovarvara@gmail.com>
2020-04-29 01:58:08 +0300

    feat: start resolving objects` tasks

--- src/06-objects-tasks.js
+++ src/06-objects-tasks.js
@@ -20,8 +20,12 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  return {
+    width,
+    height,
+    getArea: () => width * height,
+  };
 }
 
 
@@ -35,8 +39,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(object) {
+  return JSON.stringify(object);
 }
 
 
@@ -51,8 +55,13 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const data = JSON.parse(json);
+  const instance = Object.create(proto);
+  Object.keys(data).forEach((key) => {
+    instance[key] = data[key];
+  });
+  return instance;
 }
 
 
